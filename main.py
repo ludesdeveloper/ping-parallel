1c68cc649e5129326ef01c7b0e91aee706694385
FunGuardian <seski.ramadhan@gmail.com>
2021-12-04 01:37:11 +0700

    rename, update results

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
 
+# ping function
+
 
 def ping(ip_prefix, i):
     response = subprocess.call(["ping", "-c", "1", ip_prefix+"."+str(i)])
@@ -12,8 +14,10 @@ def ping(ip_prefix, i):
     if response == 0:
         ip_status = ip_prefix+"."+str(i)+" is Up"
     else:
-        ip_status = ip_prefix+"."+str(i)+" is Down"
+        ip_status = ip_prefix+"."+str(i)+"is Down"
     return ip_status
+
+# concurrent function
 
 
 def concurrent_action(ip_prefix, workers):
@@ -21,14 +25,11 @@ def concurrent_action(ip_prefix, workers):
     with ThreadPoolExecutor(max_workers=workers) as executor:
         futures = [executor.submit(ping, ip_prefix, i)
                    for i in range(1, 255)]
-        print(futures)
         for future in futures:
             try:
                 results.append(future.result())
             except TypeError as e:
                 print(e)
-    for i in results:
-        print(i)
     return results
 
 
@@ -37,14 +38,31 @@ if arguments[2] == "c":
     results = concurrent_action(arguments[1], int(arguments[3]))
     write = open("result.txt", "w")
     for i in results:
-        write.write(i+"\n")
+        if "Up" in i:
+            write.write(i+"\n")
     write.close()
 elif arguments[2] == "b":
+    results = []
     for i in range(1, 255):
-        results = concurrent_action(arguments[1]+"."+str(i), int(arguments[3]))
-        print(results)
+        get_result = concurrent_action(
+            arguments[1]+"."+str(i), int(arguments[3]))
+        results.append(get_result)
+    write = open("result.txt", "w")
+    for result in results:
+        for i in result:
+            if "Up" in i:
+                write.write(i+"\n")
+    write.close()
 elif arguments[2] == "a":
-    for a in range(1, 255):
-        for b in range(1, 255):
-            concurrent_action(arguments[1]+"." +
-                              str(a)+"."+str(b), int(arguments[3]))
+    results = []
+    for a in range(1, 3):
+        for b in range(1, 3):
+            get_result = concurrent_action(
+                arguments[1]+"."+str(a)+"."+str(b), int(arguments[3]))
+            results.append(get_result)
+    write = open("result.txt", "w")
+    for result in results:
+        for i in result:
+            if "Up" in i:
+                write.write(i+"\n")
+    write.close()
